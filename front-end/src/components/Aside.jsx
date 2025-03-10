1188bd35dffb508ecef09588e2f73e894bbc2781
Gustavo Cardoso Costa <ritacardoso290@gmail.com>
2025-03-09 22:12:15 -0300

    up

--- front-end/src/components/Aside.jsx
+++ front-end/src/components/Aside.jsx
@@ -1,18 +1,56 @@
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import brasileirinho from "../assets/logo/brasileirinho.jpeg";
+import maistocadas from "../assets/logo/mais.jpeg";
+import music from "../assets/logo/2025.jpeg";
 
 const Aside = () => {
   return (
     <div className="aside">
-      <p>Sua Biblioteca</p>
+      <div className="aside__title">
+        <FontAwesomeIcon className="asite-title__icon"  icon={faBookmark} fade style={{color: "#a5a5a7",}}/>
+        <p>Sua Biblioteca</p>
+      </div>
 
-      <div className="aside__block"></div>
-      <div className="aside__block"></div>
-      <div className="aside__block"></div>
-      <div className="aside__block"></div>
-      <div className="aside__block"></div>
-      <div className="aside__block"></div>
-      <div className="aside__block"></div>
-      <div className="aside__block"></div>
+      <div className="aside__items">
+        <div className="aside__block">
+          <img src={brasileirinho} alt='Capa da Playlist' />
+          <p>Brasileirinho</p>
+        </div>
+        <div className="aside__block">
+          <img src={maistocadas} alt='Capa da Playlist' />
+          <p>Mais tocadas</p>
+        </div>
+        <div className="aside__block">
+          <img src={music} alt='Capa da Playlist ' />
+          <p>Mais Ouvidas 2025</p>
+        </div>
+        <div className="aside__block">
+          <img src={brasileirinho} alt='Capa da Playlist' />
+          <p>Brasileirinho</p>
+        </div>
+        <div className="aside__block">
+          <img src={maistocadas} alt='Capa da Playlist' />
+          <p>Mais tocadas</p>
+        </div>
+        <div className="aside__block">
+          <img src={music} alt='Capa da Playlist ' />
+          <p>Mais Ouvidas 2025</p>
+        </div>
+        <div className="aside__block">
+          <img src={brasileirinho} alt='Capa da Playlist' />
+          <p>Brasileirinho</p>
+        </div>
+        <div className="aside__block">
+          <img src={maistocadas} alt='Capa da Playlist' />
+          <p>Mais tocadas</p>
+        </div>
+        <div className="aside__block">
+          <img src={music} alt='Capa da Playlist ' />
+          <p>Mais Ouvidas 2025</p>
+        </div>
+      </div>
+
     </div>
   )
 }
